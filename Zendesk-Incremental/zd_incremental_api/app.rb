43e0f9682a342725295defc3523bbd64dddf0d0b
DeVaris Brown <devaris@devaris.com>
2014-02-07 11:22:14 -0800

    update gitignore, change average method, add threading to metrics update

--- Zendesk-Incremental/zd_incremental_api/app.rb
+++ Zendesk-Incremental/zd_incremental_api/app.rb
@@ -4,21 +4,13 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/base'
 require 'data_mapper'
+require './bin/update_metrics'
 
 require 'dotenv'
 Dotenv.load
 
 class MyApp < Sinatra::Base
 	DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/incremental")
-
-	class Average
-		include DataMapper::Resource
-
-		property :id, Serial
-		property :wait_time, String
-	  property :reply_time, String
-	  property :resolution_time, String
-	end
 
 	class Ticket
 	  include DataMapper::Resource
@@ -37,19 +29,20 @@ class MyApp < Sinatra::Base
 	end
 
 	get '/wait_time' do
-		@wait_time = Average.last(:wait_time)
+    # compute averages based on field and store in averages table
+    @wait_time = Ticket.avg(:wait_time)
 
     render json: @wait_time
   end
 
   get '/reply_time' do
-		@reply_time = Average.last(:reply_time)
+		@reply_time = Ticket.avg(:reply_time)
 
     render json: @reply_time
   end
 
   get '/resolution_time' do
-		@resolution_time = Average.last params(:resolution_time)
+		@resolution_time = Ticket.avg(:resolution_time)
 
     render json: @resolution_time
   end
